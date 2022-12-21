bebbd829abe2fffd911709d4eec66809e9308ee6
now-and-here <120262370+now-and-here@users.noreply.github.com>
2022-12-21 13:50:34 -0500

    Update server.js

--- server.js
+++ server.js
@@ -25,8 +25,7 @@ app.listen(port, () => console.log(`Server listening on port ${port}`));
 
 function respondStatic(req, res) {
   const filename = req.url.split("/static/")[1];
-  console.log(filename)
-  fs.createReadStream(filename)
+  fs.createReadStream("/public/"+filename)
     .on("error", () => respondNotFound(req, res))
     .pipe(res);
 }
