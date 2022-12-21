3dcad9b552f6f7f6db5ce5812e080b7ce51bffbf
now-and-here <120262370+now-and-here@users.noreply.github.com>
2022-12-20 22:27:22 -0500

    Update server.js

--- server.js
+++ server.js
@@ -46,6 +46,6 @@ function respondSSE(req, res) {
 }
 
 function respondNotFound(req, res) {
-  res.writeHead(404, { "Content-Type": "text/plain" });
+  res.writeHead(404, { "Content-Type": "text/html" });
   res.end("Not Found");
 }
